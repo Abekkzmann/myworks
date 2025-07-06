36c3bb33a5b264189dcd7e8a893efa1e3a71c331
Abekkzmann <ras_quasi@yahoo.com>
2025-07-06 02:33:56 +0000

    another changes is made

--- hello_world.py
+++ hello_world.py
@@ -1,1 +1,13 @@
 # hello_world
+Installs a plugin either from a file, an URL, or from update center.
+
+ SOURCE    : If this points to a local file, that file will be installed. If
+             this is an URL, Jenkins downloads the URL and installs that as a
+             plugin.Otherwise the name is assumed to be the short name of the
+             plugin in the existing update center (like "findbugs"),and the
+             plugin will be installed from the update center.
+ -deploy   : Deploy plugins right away without postponing them until the reboot.
+ -name VAL : If specified, the plugin will be installed as this short name
+             (whereas normally the name is inferred from the source name
+             automatically).
+ -restart  : Restart Jenkins upon successful installation.
